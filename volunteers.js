85025d422792be083ec5208f0e72873b032eced2
bgoff4 <bgoff4@capellauniversity.edu>
2025-03-09 19:39:03 -0500

    Add files via upload

--- volunteers.js
+++ volunteers.js
@@ -1,82 +1,38 @@
-var $ = function (id) { return document.getElementById(id); };
+document.addEventListener('DOMContentLoaded', function() {
+    const addButton = document.getElementById('add_button');
+    const deleteButton = document.getElementById('delete_button');
+    const clearButton = document.getElementById('clear_button');
+    const sortButton = document.getElementById('sort_button');
+    const volunteerList = document.getElementById('volunteerList');
+    let volunteers = [];
 
-var volunteerArray = [];
-
-var displayVolunteers = function () {   
-    // display the volunteers in the text area
-    // $("volunteerList").value = volunteerArray.join("\n");
-
-    // comment out the line above change this to a loop instead to loop through the array.
-    var volunteerList = "";
-    for (var i = 0; i < volunteerArray.length; i++) {
-        volunteerList += volunteerArray[i] + "\n";
-    }
-    $("volunteerList").value = volunteerList;
-};
-
-var addVolunteer = function () {
-    // get the data from the form
-    var volunteerString = $("first_name").value + " " + $("last_name").value;
-
-    // store the data in an array
-    volunteerArray.push(volunteerString);
-    
-    // display the volunteers and clear the add form
-    displayVolunteers();
-    
-    // get the add form ready for next entry
-    $("first_name").value = "";
-    $("last_name").value = "";
-    $("first_name").focus();
-};
-
-var deleteVolunteer = function () {
-    // get the data from the form (hint: use the same format as from the add).
-    var volunteerString = $("first_name").value + " " + $("last_name").value;
-
-    // remove the string from the array (hint, loop through the entire list, compare the string with the item in the array.
-    for (var i = 0; i < volunteerArray.length; i++) {
-        if (volunteerArray[i] === volunteerString) {
-            volunteerArray.splice(i, 1);
-            break;
+    addButton.addEventListener('click', function() {
+        const firstName = document.getElementById('first_name').value.trim();
+        const lastName = document.getElementById('last_name').value.trim();
+        if (firstName && lastName) {
+            volunteers.push(`${firstName} ${lastName}`);
+            updateVolunteerList();
         }
+    });
+
+    deleteButton.addEventListener('click', function() {
+        const firstName = document.getElementById('first_name').value.trim();
+        const lastName = document.getElementById('last_name').value.trim();
+        volunteers = volunteers.filter(volunteer => volunteer !== `${firstName} ${lastName}`);
+        updateVolunteerList();
+    });
+
+    clearButton.addEventListener('click', function() {
+        volunteers = [];
+        updateVolunteerList();
+    });
+
+    sortButton.addEventListener('click', function() {
+        volunteers.sort();
+        updateVolunteerList();
+    });
+
+    function updateVolunteerList() {
+        volunteerList.value = volunteers.join('\n');
     }
-
-    // display the volunteers and clear the add form
-    displayVolunteers();
-    
-    // get the delete form ready for next entry
-    $("first_name").value = "";
-    $("last_name").value = "";
-    $("first_name").focus();
-};
-
-var clearList = function () {   
-    // delete the data from the arrays
-    volunteerArray = [];
-    
-	//   alternative way to delete all of the data from the array
-	//    volunteerArray.length = 0;
-    
-    // remove the volunteers data from the web page
-    $("volunteerList").value = "";
-    
-    $("first_name").focus();
-};
-
-var sortList = function () {   
-    // sort the scores
-    volunteerArray.sort();
-    
-    // display the scores
-    displayVolunteers();    
-};
-
-//When the page is fully loaded, the buttons will be mapped to the JavaScript functions
-window.onload = function () {
-    $("add_button").onclick = addVolunteer;
-	$("delete_button").onclick = deleteVolunteer;
-    $("clear_button").onclick = clearList;    
-    $("sort_button").onclick = sortList;    
-    $("first_name").focus();
-};
+});
